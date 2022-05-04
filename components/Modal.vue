672de2d18c7ad7865c90eac4709c958d77aa20af
Fraser Watt <fsrwatt@gmail.com>
2022-05-04 14:40:12 +0100

    accessibility

--- components/Modal.vue
+++ components/Modal.vue
@@ -4,7 +4,7 @@
     <h1 class="sr-only">Rating Modal</h1>
     <div class="[ modal-content ] [ flow flex-col pad-top-600 pad-bottom-600 pad-right-600 pad-left-600 ]" v-if="!submitted">
       <div class="[ star-container ] [ circular flex-row flex-center ]">
-        <img src="/icon-star.svg" alt="">
+        <img src="/icon-star.svg" alt="" aria-hidden="true">
       </div>
       <h2 class="font-white">How did we do?</h2>
       <p class="font-light-grey">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
@@ -26,7 +26,7 @@
     </div>
     <div class="[ modal-content ] [ flow flex-col flex-center pad-top-700 pad-bottom-700 pad-right-700 pad-left-700 ]" v-else>
       <div class="image-container">
-        <img src="/illustration-thank-you.svg" alt="">
+        <img src="/illustration-thank-you.svg" alt="Phone paying transaction">
       </div>
       <p class="[ modal-rating-text ] [ bg-dark-blue font-orange ]">You selected {{ rating }} out of 5</p>
       <h2 class="[ font-white ]">Thank you!</h2>
